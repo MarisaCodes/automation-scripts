e8a7a8a8efdb54b034c589468c245a2d8b55f8fe
MarisaCodes <marisakirisame955@gmail.com>
2023-08-24 00:25:43 +0400

    added .pylintrc

--- scripts/tsrollup/tsrollup.py
+++ scripts/tsrollup/tsrollup.py
@@ -1,3 +1,5 @@
+import os
+
 # ANSI color codes:
 RED="\x1b[0;31m"
 GREEN="\x1b[0;32m"
@@ -5,16 +7,14 @@ BLUE="\x1b[0;34m"
 YELLOW="\x1b[0;33m"
 RESET="\x1b[0;m"
 
-import os as os
 
 def check_pkgjson()->(bool|None):
     cwd = os.getcwd()
     pkgjson_path = os.path.abspath(cwd+"/package.json")
     is_pkgjson = os.path.exists(pkgjson_path)
-    if (is_pkgjson):
+    if is_pkgjson:
         return True
-    else:
-        print(f"{RED}Error: {GREEN}package.json{RESET} does not exist in {BLUE+cwd+RESET}\n\tFile {BLUE}{pkgjson_path}{RESET} does not exist")
-        return
-    
-    
+    print(f"{RED}Error: {GREEN}package.json{RESET} does not exist in {BLUE+cwd+RESET}\n\tFile {BLUE}{pkgjson_path}{RESET} does not exist")
+    return
+
+check_pkgjson()
